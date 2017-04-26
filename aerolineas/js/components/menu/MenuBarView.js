67b9f8c5d0c95242b022b4fa60c7d14be54d18c5
mlucero <rml2287@gmail.com>
2017-04-26 11:02:11 -0300

    Adding menu to page

---
+++ aerolineas/js/components/menu/MenuBarView.js
@@ -0,0 +1,64 @@
+/*var config = {
+    "type": "horizontal",
+    "el": "",
+    "class": ["", ""], //optional
+    "items": [{
+        "text": "",
+        "href": "",
+        "target": "",
+        "dropdown": [{ //optional
+            "text": "",
+            "href": "",
+            "target": ""
+        }]
+    }]
+}*/
+
+/**
+ * 
+ * @param {*object} configs 
+ */
+var MenuBarView = function(configs) {
+    this.configs = configs;
+    this.$el = document.querySelector(configs.el);
+}
+
+/**
+ * 
+ */
+MenuBarView.prototype = {
+    /**
+     * 
+     */
+    get: function(att) {
+        return this.configs[att];
+    },
+    /**
+     * 
+     */
+    set: function(att, value) {
+        this.configs[att] = value;
+    },
+    /**
+     * 
+     */
+    createItemList: function() {
+        return this.configs.items
+            .map(item => {
+                return `<li><a href="${item.href}" target="${item.target}">${item.text}</a></li>`
+            })
+            .reduce((previousValue, currentValue) => {
+                return previousValue + currentValue;
+            }, "");
+    },
+    render: function() {
+        let itemList = this.createItemList();
+        let template = `<nav class="cols left md-12"> 
+                    <ul class="display-inline">
+                        ${itemList}
+                    </ul>
+                </nav>`;
+
+        this.$el.innerHTML = template;
+    }
+}
